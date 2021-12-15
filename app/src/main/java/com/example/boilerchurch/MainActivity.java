7e13be31b71346b53043c6639ab9a178133bdd8c
andrewlesh <andrewfire77@gmail.com>
2021-12-15 20:42:23 +0300

    receiver

--- app/src/main/java/com/example/boilerchurch/MainActivity.java
+++ app/src/main/java/com/example/boilerchurch/MainActivity.java
@@ -11,6 +11,7 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.util.AndroidException;
 import android.view.View;
 import android.widget.Button;
 import static android.Manifest.permission.CALL_PHONE;
@@ -18,21 +19,30 @@ import static android.Manifest.permission.CALL_PHONE;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     Button getInfo, setInfo;
 
+
     private static final int REQUEST_CALL = 1;
-    String number = "<your number>";
+    String number = "+79536610669";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         getInfo = (Button) findViewById(R.id.get_info);
         setInfo = (Button) findViewById(R.id.set_info);
+
+        if (!checkPermission(Manifest.permission.RECEIVE_SMS)) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECEIVE_SMS}, 222);
+        }
 
         getInfo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-        
+
         setInfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -82,7 +92,9 @@ public class MainActivity extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
-    public void getNotify() {
-
+    private boolean checkPermission(String permission) {
+        int checkPermission = ContextCompat.checkSelfPermission(this, permission);
+        return (checkPermission == PackageManager.PERMISSION_GRANTED);
     }
+
 }
